15110c37490d6184eabb37456a204edeed89fbe0
Patryk Peszko <patryk.peszko@xing.com>
2009-12-09 22:47:58 +0100

    removing debug output

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,6 +1,5 @@
 module NotificationsHelper
   def sorted_output(enum)
-    Rails.logger.error enum.inspect
     return [] unless enum
     array = enum.inject([]) do |memo, (key, value)| 
       memo << [key, value]
