1d0e519b31e579b950b3d8f509d21d8a3f6b9b24
Patryk Peszko <patryk.peszko@xing.com>
2010-02-24 11:03:59 +0100

    easy bookmarking

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def title
+    if @notifications
+      h truncate("[#{@project.name}] #{@notifications.first.identifier}", 80)
+    else
+      "Exceptions Begone"
+    end
+  end
 end
